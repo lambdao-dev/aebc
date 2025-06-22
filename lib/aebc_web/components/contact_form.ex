3e955a8740f4effb9054f18bd0152e5b6b53fb30
len <len@lambdao.dev>
2025-06-22 20:20:13 +0200

    make contact form more compact in views

--- lib/aebc_web/components/contact_form.ex
+++ lib/aebc_web/components/contact_form.ex
@@ -5,20 +5,33 @@ defmodule AebcWeb.ContactForm do
 
   attr :current_path, :string, required: true
   attr :class, :string, default: nil
+  attr :compact, :boolean, default: false
 
   def contact_form(assigns) do
     ~H"""
-    <div class={["mt-8 p-6 bg-white rounded-lg shadow-md max-w-88", @class]}>
-      <h2 class="text-xl font-semibold mb-4"><%= gettext("Contact Us") %></h2>
+    <div class={[
+      "bg-white rounded-lg shadow-md max-w-88",
+      @compact && "p-3",
+      !@compact && "mt-8 p-6",
+      @class
+    ]}>
+      <h2 class={[
+        "font-semibold mb-4",
+        @compact && "text-lg",
+        !@compact && "text-xl"
+      ]}><%= gettext("Contact Us") %></h2>
       <.form
         :let={f}
         for={%{}}
         as={:contact}
         phx-submit="submit_contact"
-        class="space-y-4"
+        class={[
+          "space-y-4",
+          @compact && "space-y-2"
+        ]}
       >
         <input type="hidden" name="contact[state]" value="open" />
-        <input type="hidden" name="contact[page]" value={@current_path} />
+        <input type="hidden" name="contact[source_page]" value={@current_path} />
 
         <div>
           <.input
@@ -27,6 +40,7 @@ defmodule AebcWeb.ContactForm do
             label={gettext("Email")}
             required
             placeholder={gettext("your.email@example.com")}
+            class={@compact && "text-sm"}
           />
         </div>
 
@@ -37,12 +51,16 @@ defmodule AebcWeb.ContactForm do
             label={gettext("Your Question")}
             required
             placeholder={gettext("How can we help you?")}
-            rows="4"
+            rows={@compact && "2" || "4"}
+            class={@compact && "text-sm"}
           />
         </div>
 
         <div>
-          <.button type="submit" class="w-full">
+          <.button type="submit" class={[
+            "w-full",
+            @compact && "py-1 px-3 text-sm"
+          ]}>
             <%= gettext("Send Question") %>
           </.button>
         </div>
