ac34451f9c0a8a9868b23cdcadcad4448e801ef5
fah-mili <fanny@lambdao.dev>
2026-06-12 14:40:24 +0200

    [FIX] check_origin for production domain.

--- config/runtime.exs
+++ config/runtime.exs
@@ -61,6 +61,7 @@ if config_env() == :prod do
 
   config :aebc, AebcWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
+    check_origin: ["https://aebc-confucius.org", "https://www.aebc-confucius.org"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
